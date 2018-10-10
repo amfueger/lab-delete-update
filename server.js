3cac8becf660d286606f89095fe368130ebb831a
Ashley <ashley@Jamess-MacBook-Air.local>
2018-10-10 11:46:45 -0500

    test commit

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
-const Humans = require('.models/humans')
+const Humans = require('./models/humans');
+
+
+app.get('/humans', (req, res) => {
+	res.send(Humans);
+});
 
 
 
+
+
+
+//Listener
 app.get(3000, () => {
 	console.log('listener working');
 })
+
