0c5e86d145058def80b3cec4234b6991760805d4
Chris Roman <Christopherroman85@gmail.com>
2018-10-10 12:47:38 -0500

    created edit ejs

--- server.js
+++ server.js
@@ -17,12 +17,27 @@ app.get('/humans', (req, res) => {
 	res.render('index.ejs', {Humans});
 });
 
+app.get('/humans/:id/edit', (req, res) => {
+	res.render('edit.ejs', {
+		humans: Humans[req.params.index],
+		id: req.params.index
+	})
+})
 
 app.delete('/humans/:id', (req, res) => {
 	Humans.splice(req.params.id, 1);
 	res.redirect('/humans');
 });
 
+app.put('/humans/:id', (req, res) => {
+	if(req.body.hat === 'on') {
+		req.body.hat = true;
+	} else {
+		req.body.hat = false;
+	}
+	Humans[req.params.id] = req.body;
+	res.redirect('/humans')
+})
 
 
 //Listener
