00bb87e371a9b8733f20bb340c60eb50b0a40f1a
Ashley <ashley@Jamess-MacBook-Air.local>
2018-10-10 11:58:34 -0500

    added index, delete form

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+const methodOverride = require('method-override');
 
 const Humans = require('./models/humans');
 
 
 app.get('/humans', (req, res) => {
-	res.send(Humans);
+	res.render('index.ejs', {Humans});
 });
 
 
-
+app.delete('/humans/:id', (req, res) => {
+	Humans.splice(req.params.id, 1);
+	res.redirect('/humans');
+});
 
 
 
